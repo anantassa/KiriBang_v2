f491c79fff44e64c91c7734a5d6f89e90f45d211
Anantassa <anantasyafitri@gmail.com>
2016-12-23 02:23:34 +0700

    Fragments

--- app/src/main/java/com/example/tassa/kiribang/MainActivity.java
+++ app/src/main/java/com/example/tassa/kiribang/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tassa.kiribang;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.tassa.kiribang.fragments.AngkotFragment;
+import com.example.tassa.kiribang.fragments.BusFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -81,8 +85,13 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            AngkotFragment angkotFragment = new AngkotFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.content_main, angkotFragment, angkotFragment.getTag()).commit();
         } else if (id == R.id.nav_gallery) {
+            BusFragment busFragment = new BusFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.content_main, busFragment, busFragment.getTag()).commit();
 
         } else if (id == R.id.nav_slideshow) {
 
